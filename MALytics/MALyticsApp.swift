0408009899a410901465466640313b324d88b2c6
Cem Caylak <cem.caylak@icloud.com>
2024-11-15 10:26:25 +0100

    Initial Commit

---
+++ MALytics/MALyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MALyticsApp.swift
+//  MALytics
+//
+//  Created by Cem Caylak on 15.11.24.
+//
+
+import SwiftUI
+
+@main
+struct MALyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
