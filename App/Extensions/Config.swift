b1d483bda60af8ec365ec4ea4b240cd4c448aea9
Cem Caylak <cem.caylak@icloud.com>
2025-05-04 00:10:20 +0200

    Xcode Cloud setup

--- App/Extensions/Config.swift
+++ App/Extensions/Config.swift
@@ -1,12 +1,14 @@
 import Foundation
 
 struct Config {
-    static var apiKey: String {
-        guard let path = Bundle.main.path(forResource: "Config", ofType: "plist"),
-              let plist = NSDictionary(contentsOfFile: path),
-              let key = plist["API_KEY"] as? String else {
-            fatalError("Config.plist file is missing or MAL_API_KEY is not set")
+    private static func secrets() -> [String: Any] {
+            let fileName = "secrets"
+            let path = Bundle.main.path(forResource: fileName, ofType: "json")!
+            let data = try! Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+            return try! JSONSerialization.jsonObject(with: data) as! [String: Any]
         }
-        return key
+    
+    static var apiKey: String {
+        return secrets()["API_KEY"] as! String
     }
 }
